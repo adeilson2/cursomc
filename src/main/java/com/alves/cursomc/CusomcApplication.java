ae310cb707f390591a7250b4d98b6770f53332f6
Adeilson Alves <adeilsonqualidade@gmail.com>
2018-12-05 20:38:37 -0200

    Projeto criado

---
+++ src/main/java/com/alves/cursomc/CusomcApplication.java
@@ -0,0 +1,12 @@
+package com.alves.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CusomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CusomcApplication.class, args);
+	}
+}
